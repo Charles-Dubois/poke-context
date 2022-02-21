7896f15e283d23d4a6f7e78aad135b7bc34c826a
Charles Dubois <charles.dubois.h@gmail.com>
2022-02-21 14:20:09 +0100

    step 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,40 @@
+import { useForm } from "react-hook-form";
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
-    <>
-      <p>Login</p>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("username", {
+          required: "this is required",
+          maxLength: {
+            value: 15,
+            message: "15 charaters max",
+          },
+        })}
+        placeholder="Username"
+      />
+      {<span>{errors.username?.message}</span>}
+
+      <input
+        {...register("password", {
+          required: "this is required",
+          minLength: {
+            value: 6,
+            message: "6 charaters mininmum",
+          },
+        })}
+        placeholder="Password"
+        type="password"
+      />
+      {<span>{errors.password?.message}</span>}
+
+      <input type="submit" />
+    </form>
   );
 }
