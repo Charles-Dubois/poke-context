e22f4e9d25a61a9b4e8e077764c27bb3e324bdc0
Charles Dubois <charles.dubois.h@gmail.com>
2022-02-21 15:14:04 +0100

    step 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [pokemon, setPokemon] = useState([]);
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then((res) => res.json())
+      .then((res) => setPokemon(res))
+      .then(console.log(pokemon))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (randomNumber !== 0) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+        .then((res) => res.json())
+        .then((res) => setPokemon(res))
+        .then(console.log(pokemon))
+        .catch((err) => console.log(err));
+    }
+  }, [randomNumber]);
+
+  function ChangeRandomNumber() {
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+  }
+
   return (
     <>
-      <p>Home</p>
+      <button onClick={ChangeRandomNumber}>Click me!</button>
+      <p>Name : {pokemon.name}</p>
+      <p>Height :{pokemon.height}</p>
+      <p>Weight : {pokemon.weight}</p>
+      <p>Type : {pokemon.type}</p>
     </>
   );
 }
